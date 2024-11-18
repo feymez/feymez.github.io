1f9e3c0626758a7f9ae4f660c389dd9e2d8146af
donfeymezo <brajan@skiba.pro>
2024-11-18 22:03:03 +0100

    Responsive trial

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener('resize', () => {
+  document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
+});
+  
+
+const toggleButton = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+toggleButton.addEventListener('click', () => {
+  menu.classList.toggle('show'); // Przełącza widoczność menu
+});
