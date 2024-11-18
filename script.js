ce6df5700b65faf8b40326963bb141616ab5f7e5
donfeymezo <brajan@skiba.pro>
2024-11-18 23:03:10 +0100

    Redesigned main page, fuck responsive

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-window.addEventListener('resize', () => {
-  document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
-});
-  
+document.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('resize', () => {
+    document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
+  });
 
-const toggleButton = document.querySelector('.menu-toggle');
-const menu = document.querySelector('.menu');
+  const toggleButton = document.querySelector('.menu-toggle');
+  const menu = document.querySelector('.menu');
 
-toggleButton.addEventListener('click', () => {
-  menu.classList.toggle('show'); // Przełącza widoczność menu
+  toggleButton.addEventListener('click', () => {
+    menu.classList.toggle('show'); // Toggles visibility of the menu
+  });
 });
