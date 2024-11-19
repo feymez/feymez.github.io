c357dbf4126940a94dec0ce36260f5b52f5873a4
donfeymezo <brajan@skiba.pro>
2024-11-19 16:00:32 +0100

    First mobile menu

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
     document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
   });
 
-  const toggleButton = document.querySelector('.menu-toggle');
-  const menu = document.querySelector('.menu');
-
-  toggleButton.addEventListener('click', () => {
-    menu.classList.toggle('show'); // Toggles visibility of the menu
+  document.getElementById('menu-toggle').addEventListener('click', function() {
+    const menuItems = document.getElementById('menu-items');
+    menuItems.style.display = menuItems.style.display === 'block' ? 'none' : 'block';
   });
 });
