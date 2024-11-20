2fc1ed05d0acaaf124cd2925c57e52eac74f3d5b
donfeymezo <brajan@skiba.pro>
2024-11-20 20:38:35 +0100

    Rules page

---
+++ regulamin/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const year = new Date().getFullYear()
+  $('#currentYear').html(
+    'Wszystkie prawa zastrzeżone &copy; '+year+' - BlastRP &trade; <br><br>'
+  )
+
+  $('#en-US').html(
+    'BLASTRP IS NOT APPROVED, SPONSORED OR ENDORSED BY ROCKSTAR GAMES.'
+  )
+
+  $('#pl-PL').html(
+    'BLASTRP NIE JEST ZATWIERDZONY, SPONSOROWANY LUB WSPIERANY PRZEZ ROCKSTAR GAMES.'
+  )
+  window.addEventListener('resize', () => {
+    document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
+  });
+
+  document.getElementById('menu-toggle').addEventListener('click', function() {
+    const menuItems = document.getElementById('menu-items');
+    menuItems.style.display = menuItems.style.display === 'block' ? 'none' : 'block';
+  });
+});
